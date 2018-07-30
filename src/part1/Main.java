22f921d1f94df14b9ba4d742e40379ed5cf9c398
sergeylogos <sergey.logos@gmail.com>
2018-07-30 18:01:15 +0300

    complete part1

--- src/part1/Main.java
+++ src/part1/Main.java
@@ -24,7 +24,7 @@ public class Main {
         user2.id = 2;
         user2.weight = 97.654;
         user2.name = "kokos";
-        user2.isActivated = false;
+        user2.isActivated = true;
         user2.uniqueSymbol = '^';
         System.out.println(user2.age);
         System.out.println(user2.height);
@@ -34,7 +34,55 @@ public class Main {
         System.out.println(user2.isActivated);
         System.out.println(user2.uniqueSymbol);
 
+        int totalAge = user1.age + user2.age; // -*/
+        System.out.println(totalAge);
+        String ageAndNameConcat = user1.age + user1.name;
+        System.out.println(ageAndNameConcat);
+        String ageAgeName = user1.age + user1.age + user1.name;
+        System.out.println(ageAgeName);
+        String nameAgeAge = user1.name + user1.age + user1.age;
+        System.out.println(nameAgeAge);
 
+        System.out.println(user2.age % user1.age);
+
+
+        System.out.println(user1.age > user2.age); // 18 > 23 = false
+        System.out.println(user1.name.equals(user2.name)); // "vasya".equals("kokos") // false
+        System.out.println(user1.age != user2.age); // 18 != 23  true
+
+        System.out.println(user1.age == user2.age && user1.isActivated == user2.isActivated);
+        System.out.println(user1.age == user2.age || user1.isActivated == user2.isActivated);
+
+
+        String result;
+        if (user1.age > user2.age) {
+            result = "user1 older user2";
+        } else {
+            result = "user2 older user1";
+        }
+        System.out.println(result);
+
+        String result2;
+        if (user1.age == user2.age && user1.isActivated == user2.isActivated) {
+            result2 = "object are equal";
+        } else {
+            result2 = "object are not equal";
+
+        }
+
+        User user3 = new User();
+        user3.age = 32;
+
+
+        if (user1.age > user2.age && user1.age > user3.age) {
+            System.out.println("user 1 is older than all");
+        } else if (user2.age > user1.age && user2.age > user3.age) {
+            System.out.println("user 2 is older than all");
+        } else if (user3.age > user1.age && user3.age > user2.age) {
+            System.out.println("user 3 is older than all");
+        } else {
+            System.out.println("something go wrong");
+        }
 
     }
 }
