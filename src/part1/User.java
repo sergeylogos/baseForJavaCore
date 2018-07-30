c3a1ccd8dab2a57192228593e8de76455230f245
sergeylogos <sergey.logos@gmail.com>
2018-07-30 17:36:43 +0300

    create user class

---
+++ src/part1/User.java
@@ -0,0 +1,17 @@
+package part1;
+
+public class User {
+
+    byte age; // -128...127
+    short height; // от -32768 до 32767
+    //    int id ; //от -2147483648 до 2147483647
+    long id; // от -9223372036854775808 до 9223372036854775807
+    //    float weight = 87.5F; //от -1.4e-45f до 3.4e+38f
+    double weight; //от -4.9e-324 до 1.7e+308
+    String name;
+    boolean isActivated;
+    //    char uniqueSymbol;
+    char uniqueSymbol; //беззнаковое целое число, представляющее собой символ UTF-16 (буквы и цифры)
+
+
+}
