7e3f49ec18a94ef782ebf5f37b673e9c4b9901c6
sergeylogos <sergey.logos@gmail.com>
2018-07-30 18:42:53 +0300

    add part4

--- src/part3/Main.java
+++ src/part3/Main.java
@@ -11,8 +11,13 @@ public class Main {
         System.out.println(numbers[0]);
         System.out.println(numbers[1]);
         System.out.println(numbers[2]);
-        System.out.println(numbers[3]); //!!!!!!!!!
-
+//        System.out.println(numbers[3]); //!!!!!!!!! =>
+        try {
+            System.out.println(numbers[3]);
+        } catch (Exception e) {
+            System.out.println("some go wrong");
+        }
+        System.out.println("that`s all folks");
 
         User user1 = new User("vasya", 100);
         User user2 = new User("petya", 200);
@@ -23,23 +28,36 @@ public class Main {
                 user2,
                 user3
         };
-        System.out.println(userList[0]);
-        System.out.println(userList[1]);
-        System.out.println(userList[2]);
+        System.out.println(userList[0].name);
+        System.out.println(userList[1].name);
+        System.out.println(userList[2].name);
 
         User[] users = new User[3];
         users[0] = user1;
-        System.out.println(users[0]);
-        System.out.println(users[1]);
-        System.out.println(users[2]);
         users[1] = user2;
-        System.out.println(users[0]);
-        System.out.println(users[1]);
-        System.out.println(users[2]);
         users[2] = user3;
-        System.out.println(users[0]);
-        System.out.println(users[1]);
-        System.out.println(users[2]);
+        System.out.println(users[0].name);
+        System.out.println(users[1].name);
+        System.out.println(users[2].name);
+
+
+        int numNum[][] = {
+                {1, 2, 3}, // 00 01 02
+                {4, 5, 6}, // 10 11 12
+                {7, 8, 9, 0} // 20 21 22
+
+        };
+
+        int[][] nunum2 = new int[3][3];
+        nunum2[0][0] = 1;
+        nunum2[0][1] = 2;
+        nunum2[0][2] = 3;
+        nunum2[1][0] = 4;
+        nunum2[1][1] = 5;
+        nunum2[1][2] = 6;
+        nunum2[2][0] = 7;
+        nunum2[2][1] = 8;
+        nunum2[2][2] = 9;
 
 
     }
