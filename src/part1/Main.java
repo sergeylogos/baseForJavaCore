baccaa8587053efe59acd3e18a7e72f448742de2
sergeylogos <sergey.logos@gmail.com>
2018-07-30 17:40:08 +0300

    add Main class create user

---
+++ src/part1/Main.java
@@ -0,0 +1,16 @@
+package part1;
+
+public class Main {
+    public static void main(String[] args) {
+        User user1 = new User();
+        System.out.println(user1.age);
+        System.out.println(user1.height);
+        System.out.println(user1.id);
+        System.out.println(user1.weight);
+        System.out.println(user1.name);
+        System.out.println(user1.isActivated);
+        System.out.println(user1.uniqueSymbol);
+
+
+    }
+}
